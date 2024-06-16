2f0290f9f0976c8120e5c713333467026ee345a4
Het Patel <hetp1815@gmail.com>
2024-06-16 12:19:55 +0530

    Fix : bug of redirecting user

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,20 @@ import SideBar from "@/components/SideBar";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
-
   const user = await currentUser();
-  if (!user) {
-    return <div>loading...</div>;
-  }
 
   return (
     <div className="pt-20">
       <div className="max-w-6xl mx-auto flex justify-between gap-8">
         {/* Sidebar */}
-        <SideBar  user={user}/>
+        <SideBar user={user} />
 
         {/* Feed */}
-        <Feed user={user}/>
-        
-        {/* News */}
-        <News/>
+        <Feed user={user} />
 
-      </div> 
+        {/* News */}
+        <News />
+      </div>
     </div>
   );
 }
